f9585c6abc454539beff1e692a5c12ce0e190088
JonathanLCunnane <jonathan.cunnane@gmail.com>
2023-03-09 22:39:24 +0000

    Implemented board draw function.

--- src/ChessBoardPanel.java
+++ src/ChessBoardPanel.java
@@ -12,16 +12,17 @@ public class ChessBoardPanel extends JPanel
 {
     private final int boardSize = 512;
     private final int marginSize = 16;
+    Board chessBoard;
     BufferedImage boardImage = ImageGetter.tryGetImage("/img/board.png", getClass());
 
     ChessBoardPanel(Board board)
     {
-
+        chessBoard = board;
     }
 
     @Override
     protected void paintComponent(Graphics g) {
-        // Draw board
+        // Draw board.
         g.setColor(Color.DARK_GRAY);
 
         int extraTopMargin = (getHeight() - boardSize - (marginSize * 2))/2;
@@ -30,6 +31,33 @@ public class ChessBoardPanel extends JPanel
         extraSideMargin = Math.max(extraSideMargin, 0);
 
         g.drawImage(boardImage, marginSize + extraSideMargin, marginSize + extraTopMargin, null);
+
+        // Draw pieces.
+        for (EnumPiece piece: EnumPiece.values())
+        {
+            long currBitBoard = chessBoard.bitBoards[piece.ordinal()];
+            for (int square = 63; square >= 0; square--)
+            {
+                int row = square / 8;
+                int column = square % 8;
+                if ((currBitBoard & 1) == 1)
+                {
+                    drawPiece(g, piece, extraSideMargin, extraTopMargin, row, column);
+                }
+                currBitBoard >>= 1;
+            }
+        }
     }
+
+    private void drawPiece(Graphics g, EnumPiece piece, int extraSideMargin, int extraTopMargin, int row, int column)
+    {
+        g.drawImage(
+                PieceImages.getImage(piece),
+                marginSize + extraSideMargin + (column * 64) + 1,
+                marginSize + extraTopMargin + (row * 64) + 1,
+                null
+        );
+    }
+
 }
 
